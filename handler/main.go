43a5ef96cc94c21e81910f04e1e2fd209f6d7ca6
dong <nhnguhoc@gmail.com>
2022-06-28 10:07:50 +0700

    gin bind json

--- handler/main.go
+++ handler/main.go
@@ -7,14 +7,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type createUser struct {
+	Name string `json:"name,omitempty"`
+	Age  *int   `json:"age,omitempty"`
+}
+
+func createUserHandler(c *gin.Context) {
+	p := new(createUser)
+
+	if err := c.BindJSON(p); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v", p)
+	// fmt.Printf("age = %d  , name = %s", *p.Age, p.Name)
+	c.JSON(200, p)
+}
+
 func Run(port int) error {
+	// gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.POST("/me", Authenicated, Authenicated)
+	r.POST("/me", Authenicated, Authenicated, createUserHandler)
 
 	return r.Run(fmt.Sprintf(":%v", port))
 }
